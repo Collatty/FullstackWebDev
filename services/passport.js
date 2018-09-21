a1a28cc8c2a7a5c4075a9d65cec2bc282c9b97ec
Collatty <hakoncb@gmail.com>
2018-09-21 13:34:29 +0200

    Avoiding multiple db entries

--- services/passport.js
+++ services/passport.js
@@ -13,7 +13,17 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //profile id in db
+          done(null, existingUser);
+        } else {
+          //create new record
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
